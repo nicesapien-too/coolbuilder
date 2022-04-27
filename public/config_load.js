c0edef54a813c40e3af02966c1fce3daa5236974
an-star-art <79096599+an-star-art@users.noreply.github.com>
2022-04-27 17:55:24 +0530

    Create config_load.js

---
+++ public/config_load.js
@@ -0,0 +1,18 @@
+    function load() {  
+const getName = document.getElementsByClassName("nam");
+      const justName = document.getElementsByClassName("nami");
+      const constHowGuy = document.getElementsByClassName("howGuy");
+      for (let countit = 0; countit < constHowGuy.length; countit++) {
+        constHowGuy[countit].innerHTML = guyType;
+      }
+      for (let count = 0; count < justName.length; count++) {
+        justName[count].innerHTML = name;
+      }
+      for (let counting = 0; counting < getName.length; counting++) {
+        getName[counting].innerHTML = name + "The" + guyType;
+      }
+      // SET PAGE TITLE
+      let title_el = document.querySelector("title");
+      if (title_el) {
+        title_el.innerHTML = name + "The" + guyType;
+      }}

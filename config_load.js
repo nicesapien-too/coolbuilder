d46a7a01e9184b2ccd54c4608ea7f10b64bf8062
an-star-art <79096599+an-star-art@users.noreply.github.com>
2022-04-29 15:09:22 +0530

    Invisible contactus button if no email specified

--- config_load.js
+++ config_load.js
@@ -20,5 +20,5 @@ function configContact() {
   if (!contact_email == "") {
     contactElem.href = 'mailto:' + contact_email;
   } else {
-    contactElem.style.display = 'none';
+    document.getElementById("contact").style.display = 'none';
   }}

29ef16501e0f7e0dc2515e4a431551b1f56a0718
an-star-art <79096599+an-star-art@users.noreply.github.com>
2022-04-29 14:37:47 +0530

    Extra comments for configuration

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
     /* EDIT THIS*/
+// Your name, real or Nick
 const name = "Nice";
-const guyType = "Cool"
-const contact_email = "email@outlook.com";
+// What you are? Cool? Legend? Best? or anything else
+const guyType = "Cool";
+// Entera contact email. Leave empty If you don't want to show a Contact Us button.
+const contact_email = "";
+/* CONFIGURATION IS AS SIMPLE AS THAT!*/

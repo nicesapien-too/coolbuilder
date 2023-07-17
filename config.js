18aa9e83891a39d1078bd6ea562c2fb8c67e099d
nicesapien-too <satlujfillingstation1@gmail.com>
2023-07-18 00:09:49 +0530

    Work on some PageSpeed insights

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
    /* EDIT THIS*/
 // Your name, real or Nick
-const name = "Nice";
+const name = "NiceSapien";
+// The middle word. You can set it to anything!
+const MiddleWord = ", Legend. ";
 // What you are? Cool? Legend? Best? or anything else
-const guyType = "Cool";
+const guyType = "Certified.";
 // What's the relationship? Friend or Bro suits best. See and read the website carefully and you'll find where relationship is used.
 const relationship = "friend";
+// Description. It will appear below the name amd stuff. You can include HTML like <b> in it.
+const description = "<b>I am committed to staying up-to-date with the latest trends</b> and technologies in software development to ensure that my clients receive the best possible solutions. I am always looking for new challenges and opportunities to grow as a developer, and I believe that my passion for software development is reflected in the quality of my work."
 // Enter a contact email. Leave empty If you don't want to show a Contact Us button.
 const contact_email = "";
 /* CONFIGURATION IS AS SIMPLE AS THAT!*/
--- config_load.js
+++ config_load.js
@@ -2,6 +2,7 @@ const getName = document.getElementsByClassName("nam");
 const justName = document.getElementsByClassName("nami");
 const constHowGuy = document.getElementsByClassName("howGuy");
 const relation = document.getElementsByClassName("relation");
+const Middleware = document.getElementsByClassName("MiddleWord");
 const contactElem = document.getElementById("contact");
 
 for (let countit = 0; countit < constHowGuy.length; countit++) {
@@ -14,8 +15,10 @@ for (let counted = 0; counted < relation.length; counted++) {
   relation[counted].innerHTML = relationship;
 }
 for (let counting = 0; counting < getName.length; counting++) {
-  getName[counting].innerHTML = name + "The" + '<b class="grade">'+guyType+"</b>";
+  getName[counting].innerHTML = name + MiddleWord + '<b class="grade">'+guyType+"</b>";
 }
+// SET THE PAGE DESCRIPTION
+document.getElementById("description").innerHTML = description;
 // SET PAGE TITLE
 let title_el = document.querySelector("title");
 if (title_el) {
